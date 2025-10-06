40ac471800a9cb9ce30691f606b045067388c0e5
John Axel De Leon <cedrickcontreras100@gmail.com>
2025-10-07 06:01:12 +0800

    STUDENTS PROGRESS TRACKER

--- practice14.py
+++ practice14.py
@@ -1,14 +1,20 @@
 name = input("Whats your name: ")
 num = eval(input("put a number of students: "))
 
+pas = 0
+fail = 0
 print(f"Welcome ma'am {name}")
 for i in range(1, num + 1):
     grade = eval(input("put the Grade of the students: "))
     if grade >= 75:
         print(f"You passed {grade}")
+        pas += 1
     else:
         print(f"Need Improvements {grade}")
+        fail += 1
 print("All students are successfully recorded")
+print(f"Students pass grade {pas}")
+print(f"Students fail grade {fail}")
 
 
 
