38f179aa226149aec49f13463f6be3096324ac81
John Axel De Leon <cedrickcontreras100@gmail.com>
2025-10-06 16:49:18 +0800

    SIMPLE ARITHMETIC

---
+++ practice4.py
@@ -0,0 +1,12 @@
+num1 = eval(input("Put a number: "))
+num2 = eval(input("Put a number: "))
+
+sum = num1 + num2
+diff = num1 - num2
+pr = num1 * num2
+quo = num1 / num2
+
+print(f"Sum = {sum}")
+print(f"Difference = {diff}")
+print(f"Product = {pr}")
+print(f"Quotient = {quo:.2f}")
