85fbecd66da8787fc0513b3ed204db1556d19b7a
John Axel De Leon <cedrickcontreras100@gmail.com>
2025-10-07 05:56:34 +0800

    STUDENTS PROGRESS TRACKER

--- practice14.py
+++ practice14.py
@@ -1,5 +1,7 @@
+name = input("Whats your name: ")
 num = eval(input("put a number of students: "))
 
+print(f"Welcome ma'am {name}")
 for i in range(1, num + 1):
     grade = eval(input("put the Grade of the students: "))
     if grade >= 75:
