2a08c792e5c2c161d89aa82fecb219fd4e52b821
John Axel De Leon <224013364+JohnAxel04@users.noreply.github.com>
2025-10-08 05:22:22 +0800

    NUMBER PYRAMID WITH TWIST

---
+++ practice18.py
@@ -0,0 +1,12 @@
+#Pyramid with the twist
+
+for i in range(1,7):
+    for u in range(1,i):
+        print(u,"*",end=" ")
+    print()
+for j in range(6,1,-1):
+    for l in range(1,j,1):
+        print(l,"*",end=" ")
+    print()
+
+
